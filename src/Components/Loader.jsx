0e4c4ec418f5125c6c8d5fa6e7dbdb833891bca6
mr2space <princegoswami.space@gmail.com>
2023-11-19 08:55:36 +0530

    error again

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <>
+    
+    hwell</>
+  )
+}
+
+export default Loader
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Loader } from '../Components/Loader'
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      <Canvas className='w-full h-screen bg-transparent' camera={{near:0.1, far:1000}} />
+
+      <Suspense fallback={<Loader />}>
+
+      </Suspense>
+    </section>
   )
 }
 
